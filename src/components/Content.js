2a1874199f865d4d8ae4e236d916e469bca7e53f
Aditya-040 <as0730728@gmail.com>
2023-12-11 00:33:46 +0530

    working,ui changed

---
+++ src/components/Content.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = () => {
+  return (
+    <div className='Con1'>
+        <h1>AI NFT Generator and Minter</h1>
+    
+       <img src="src\components\asset\sct.png" alt="" />
+    </div>
+   
+  )
+}
+
+export default Content
